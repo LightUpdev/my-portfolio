318702ea5d53aae9e8cbaca3807f09aeabcf019e
light_dev <dgr8steve95@gmail.com>
2024-10-03 13:18:48 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
+  content: ['./src/**/*.{js,jsx,ts,tsx,html}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
